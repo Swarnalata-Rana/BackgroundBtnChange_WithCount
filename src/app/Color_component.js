885fe2fea7f12d20cd785b6c5fcc0aa63616d8c9
Swarnalata Rana <swarnalata21@navgurukul.org>
2023-08-08 16:50:51 +0530

    firstcommit

---
+++ src/app/Color_component.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Color_component = () => {
+    const onClickRedBtn=(e)=>{
+      console.log(e)
+    }
+    const onClickyelloBtn=(e)=>{
+      console.log(e)
+    }
+    const onClickblueBtn=(e)=>{
+      console.log(e)
+    }
+    const onClickgreenBtn=(e)=>{
+      console.log(e)
+    }
+  return (
+    <div>        
+        <button className='redbtn' onClick={()=>{onClickRedBtn="red"}}>RedBtn</button>
+        <button className='yellobtn' onClick={()=>{onClickyelloBtn="yello"}}>YelloBtn</button>
+        <button className='bluebtn' onClick={()=>{onClickblueBtn="blue"}}>BlueBtn</button>
+        <button className='greenbtn' onClick={()=>{onClickgreenBtn="green"}}>GreenBtn</button>
+    </div>
+  )
+}
+
+export default Color_component
